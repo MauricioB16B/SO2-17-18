0b593aa028ead20d572e7e3e65855ad81add2289
MauricioB16B <39094260+MauricioB16B@users.noreply.github.com>
2018-05-09 00:33:06 +0100

    quase bom

--- interface/inter/inter/Source.cpp
+++ interface/inter/inter/Source.cpp
@@ -15,32 +15,44 @@ void main() {
 	
 	mapa[0][0] = 1;
 	mapa[1][1] = 1;
-	mapa[20][20] = 1;
+	mapa[19][19] = 1;
 	mapa[10][10] = 1;
-	mapa[5][15] = 1;
+	mapa[5][19] = 1;
 	
-	interfacee(20,20);
+	interfacee(30,25);
 }
 
 int interfacee(int x, int y) {
 	int e, i,t=0;
-	for (e = -1;e<=y+1;e++) {
-		for (i = -1;i<=x+1;i++) {
+	for (e = -2;e<y+1;e++) {
+		for (i = -1;i<x+1;i++) {
 			t = 0;
+			if (e==-2 && t == 0) {
+				if(i<10 && i>-1)
+					printf("%d  ",i);
+				if(i>=10 && i<x)
+					printf("%d ", i);
+				if (i <= -1 || i >= x)
+					printf("   ");
+				t = 1;
+			}
 			if (e==-1 && t == 0) {
 				printf("___");
 				t = 1;
 			}
-			if (e > y && t == 0) {
+			if (e >= y && t == 0) {
 				printf("---");
 				t = 1;
 			}
 			if (i == -1 && t == 0) {
-				printf("|||");
+				if(e<10)
+					printf("%d |",e);
+				if(e>=10)
+					printf("%d|", e);
 				t = 1;
 			}
-			if (i > x && t == 0) {
-				printf("|||");
+			if (i >= x && t == 0) {
+				printf("| |");
 				t = 1;
 			}
 			if (t==0) {
