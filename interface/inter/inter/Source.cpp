c801969f0aa3efe5628761f4ccd9bdc5120a14fb
MauricioB16B <39094260+MauricioB16B@users.noreply.github.com>
2018-05-08 20:21:15 +0100

    up

--- interface/inter/inter/Source.cpp
+++ interface/inter/inter/Source.cpp
@@ -5,7 +5,20 @@ int interfacee(int x, int y);
 int mapa[20][20];
 
 void main() {
+	int i,e;
+
+	for (i = 0;i <= 20;i++) {
+		for (e = 0;e <= 20;e++) {
+			mapa[i][e] = 0;
+		}
+	}
+	
 	mapa[0][0] = 1;
+	mapa[1][1] = 1;
+	mapa[20][20] = 1;
+	mapa[10][10] = 1;
+	mapa[5][15] = 1;
+	
 	interfacee(20,20);
 }
 
@@ -31,7 +44,7 @@ int interfacee(int x, int y) {
 				t = 1;
 			}
 			if (t==0) {
-				if (mapa[e][i] == 1) {
+				if (mapa[i][e] == 1) {
 					printf(" * ");
 				}
 				else{
