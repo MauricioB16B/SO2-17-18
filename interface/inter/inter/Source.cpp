153cc1db71b39b80b51643a4393a5ca89cf7d420
MauricioB16B <39094260+MauricioB16B@users.noreply.github.com>
2018-05-08 20:12:12 +0100

    upload mapa

--- interface/inter/inter/Source.cpp
+++ interface/inter/inter/Source.cpp
@@ -2,7 +2,10 @@
 
 int interfacee(int x, int y);
 
+int mapa[20][20];
+
 void main() {
+	mapa[0][0] = 1;
 	interfacee(20,20);
 }
 
@@ -20,15 +23,20 @@ int interfacee(int x, int y) {
 				t = 1;
 			}
 			if (i == -1 && t == 0) {
-				printf(" | ");
+				printf("|||");
 				t = 1;
 			}
 			if (i > x && t == 0) {
-				printf(" | ");
+				printf("|||");
 				t = 1;
 			}
 			if (t==0) {
-				printf(" * ");
+				if (mapa[e][i] == 1) {
+					printf(" * ");
+				}
+				else{
+					printf("   ");
+				}
 			}
 		}
 		printf("\n");
