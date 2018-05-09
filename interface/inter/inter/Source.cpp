3b7854f1c69ce398c9dfa92ba04fddaac5d61353
MauricioB16B <39094260+MauricioB16B@users.noreply.github.com>
2018-05-09 16:37:32 +0100

    upfinal

--- interface/inter/inter/Source.cpp
+++ interface/inter/inter/Source.cpp
@@ -1,28 +1,38 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int interfacee(int x, int y);
+#define tamx 30
+#define tamy 20
 
-int mapa[20][20];
+int interfacee(int x, int y,int **mapa);
 
 void main() {
 	int i,e;
+	//int mapa[tamx][tamy];
+	
+	int **mapa;
+	mapa = (int **)malloc(tamx * sizeof *mapa);
+	for (i = 0; i<tamx; i++){
+		mapa[i] = (int *)malloc(tamy * sizeof *mapa[i]);
+	}
 
-	for (i = 0;i <= 20;i++) {
-		for (e = 0;e <= 20;e++) {
+
+	for (i = 0;i < tamx;i++) {
+		for (e = 0;e < tamy;e++) {
 			mapa[i][e] = 0;
 		}
 	}
-	
 	mapa[0][0] = 1;
 	mapa[1][1] = 1;
-	mapa[19][19] = 1;
+	mapa[29][19] = 1;
 	mapa[10][10] = 1;
 	mapa[5][19] = 1;
+
 	
-	interfacee(30,25);
+	interfacee(tamx,tamy,mapa);
 }
 
-int interfacee(int x, int y) {
+int interfacee(int x, int y,int **mapa) {
 	int e, i,t=0;
 	for (e = -2;e<y+1;e++) {
 		for (i = -1;i<x+1;i++) {
