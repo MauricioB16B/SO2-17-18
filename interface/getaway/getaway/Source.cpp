d545c3f92bdbc1e820cb515afdb2325edfbbf73d
MauricioB16B <39094260+MauricioB16B@users.noreply.github.com>
2018-05-30 05:05:42 +0100

    uptodate

--- interface/getaway/getaway/Source.cpp
+++ interface/getaway/getaway/Source.cpp
@@ -6,7 +6,47 @@
 #include <time.h>
 #include <tchar.h>
 
-int _tmain() {
+typedef struct obj {
+	int id;
+	int tipo;
+	int x;
+	int y;
+	int tamx;
+	int tamy;
+	char bitmap[1024];
+	struct obj * prox;
+}obj;
 
+obj * mapeamento();
+
+int _tmain() {
+	obj * objectos;
+	objectos = mapeamento();
+	system("pause");
 	return 0;
+}
+
+obj * mapeamento() {
+	TCHAR syNome[] = TEXT("Global\\GlobalGameData");
+	HANDLE partilha;
+	obj *ponteiro;
+
+	partilha = OpenFileMapping(PAGE_READWRITE,
+		FALSE,
+		syNome);
+
+	if (partilha == NULL) {
+		_tprintf(TEXT("Nao foi possifel abrir o mapeamento,(%d)\nFile nao encontrado ERRO ""2""\nProblemas de permissao ERRO ""5""\n"), GetLastError());
+		return NULL;
+	}
+
+	ponteiro = (obj *)MapViewOfFile(partilha,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		sizeof(obj) * 200);
+	if (ponteiro == NULL)
+		_tprintf(TEXT("coco no mapeamento (%d)\n"),GetLastError());
+
+	return ponteiro;
 }
