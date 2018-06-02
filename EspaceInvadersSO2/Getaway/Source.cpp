e8b414b061eef70d03645761cabcf2a8868ea58b
MauricioB16B <39094260+MauricioB16B@users.noreply.github.com>
2018-06-03 00:17:50 +0100

    up

---
+++ EspaceInvadersSO2/Getaway/Source.cpp
@@ -0,0 +1,288 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <io.h>
+#include <fcntl.h>
+#include <time.h>
+#include <tchar.h>
+
+#define BufferSize 100
+#define Buffers 10
+
+
+
+typedef struct obj {
+	int id;
+	int tipo;
+	int x;
+	int y;
+	int tamx;
+	int tamy;
+	char bitmap[1024];
+	char nome[256];
+	struct obj * prox;
+}obj;
+typedef struct {
+	int tipo;
+	int aux1;
+	int aux2;
+	int aux3;
+	int aux4;
+	int aux5;
+}msg;
+typedef struct {
+
+	msg dados[Buffers];
+	int iEscrita;
+	int iLeitura;
+
+}bufferinfo;
+
+
+obj * mapeamento();
+void ler(obj *objectos);
+int buffercircular();
+int buffercircular2(int tipo, int aux1, int aux2, int aux3, int aux4, int aux5);
+
+int _tmain() {
+	int a;
+
+	while (1)
+	{
+		printf("\n	Getaway\n");
+		printf("\n	Opcoes\n\n");
+		printf("[ 1 ] -> Cria Objecto\n");
+		printf("[ 2 ] -> Cria Jogo classico\n");
+		printf("[ 3 ] -> BufferCircular\n");
+		printf("[ 4 ] -> Apaga objecto\n");
+		printf("[ 5 ] -> Sair\n");
+		scanf_s("%d", &a);
+		switch (a)
+		{
+		case 1:
+			break;
+		case 2:
+			buffercircular2(1, 1, 1, 1, 1, 1);
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			return 0;
+			break;
+		default:
+			break;
+		}
+		system("cls");
+	}
+	return 0;
+}
+
+int buffercircular() {
+	TCHAR NomeMemoria[] = TEXT("Nome da Memória Partilhada");
+	TCHAR NomeSemaforoPodeEscrever[] = TEXT("Semáforo Pode Escrever");
+	TCHAR NomeSemaforoPodeLer[] = TEXT("Semáforo Pode Ler");
+	TCHAR NomeMutexIndice[] = TEXT("MutexEscritor");
+
+	HANDLE PodeEscrever;
+	HANDLE PodeLer;
+	HANDLE hMemoria;
+	HANDLE mutex;
+
+	bufferinfo *shm;
+	int pos;
+	char init = 0;
+
+
+	PodeEscrever = CreateSemaphore(NULL, 0, Buffers, NomeSemaforoPodeEscrever);
+
+	PodeLer = CreateSemaphore(NULL, 0, Buffers, NomeSemaforoPodeLer);
+
+
+	hMemoria = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(bufferinfo), NomeMemoria);
+
+
+	mutex = CreateMutex(NULL, FALSE, NomeMutexIndice);
+
+	if (PodeEscrever == NULL || PodeLer == NULL || hMemoria == NULL) {
+		_tprintf(TEXT("[Erro]Criação de objectos do Windows(%d)\n"), GetLastError());
+		return -1;
+	}
+	if (GetLastError() != ERROR_ALREADY_EXISTS) {
+
+		init = 1;
+
+	}
+
+	shm = (bufferinfo*)MapViewOfFile(hMemoria, FILE_MAP_WRITE, 0, 0, sizeof(bufferinfo));
+
+	if (shm == NULL) {
+		_tprintf(TEXT("[Erro]Mapeamento da memória partilhada(%d)\n"), GetLastError());
+		return -1;
+	}
+
+	if (init) {
+
+		shm->iEscrita = 0;
+		shm->iLeitura = 0;
+		ReleaseSemaphore(PodeEscrever, 10, NULL);
+	}
+	for (int i = 0; i < 1; i++)
+	{
+		WaitForSingleObject(PodeEscrever, INFINITE);
+
+		//METER O MUTEX PARA PROTEGER A ESCRITA -> PODE HAVER MA INFORMACAO PARA OS CLIENTES QUANDO SE LIBERTA O SEMAFORO 
+		//DE LEITURA. ESCREVE COM INDICE > primeiro...
+		WaitForSingleObject(mutex, INFINITE);
+		//ler IN par aa var local POS
+		pos = shm->iEscrita;
+		shm->iEscrita = (shm->iEscrita + 1) % Buffers;
+		//Incrementar valor de IN
+
+
+		shm->dados[pos].tipo = 1;
+		shm->dados[pos].aux1 = 5;
+		shm->dados[pos].aux2 = 6;
+		shm->dados[pos].aux3 = 7;
+		shm->dados[pos].aux4 = 8;
+		shm->dados[pos].aux5 = 9;
+		//_stprintf_s(shm->buff[pos], BufferSize, TEXT("Pedido %d#%02d"), GetCurrentProcessId(), i);
+		//_tprintf(TEXT("Escrever para buffer %d o valor %d \n"), pos, shm->iEscrita);
+		_tprintf(TEXT("Escrever para buffer\n"));
+
+		ReleaseMutex(mutex);
+
+		Sleep(1000);
+		ReleaseSemaphore(PodeLer, 1, NULL);
+	}
+
+	UnmapViewOfFile(shm);
+	CloseHandle(PodeEscrever);
+	CloseHandle(PodeLer);
+	CloseHandle(hMemoria);
+	CloseHandle(mutex);
+	return 0;
+}
+
+int buffercircular2(int tipo, int aux1, int aux2, int aux3, int aux4, int aux5) {
+	TCHAR NomeMemoria[] = TEXT("Nome da Memória Partilhada");
+	TCHAR NomeSemaforoPodeEscrever[] = TEXT("Semáforo Pode Escrever");
+	TCHAR NomeSemaforoPodeLer[] = TEXT("Semáforo Pode Ler");
+	TCHAR NomeMutexIndice[] = TEXT("MutexEscritor");
+
+	HANDLE PodeEscrever;
+	HANDLE PodeLer;
+	HANDLE hMemoria;
+	HANDLE mutex;
+
+	bufferinfo *shm;
+	int pos;
+	char init = 0;
+
+
+	PodeEscrever = CreateSemaphore(NULL, 0, Buffers, NomeSemaforoPodeEscrever);
+
+	PodeLer = CreateSemaphore(NULL, 0, Buffers, NomeSemaforoPodeLer);
+
+
+	hMemoria = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(bufferinfo), NomeMemoria);
+
+
+	mutex = CreateMutex(NULL, FALSE, NomeMutexIndice);
+
+	if (PodeEscrever == NULL || PodeLer == NULL || hMemoria == NULL) {
+		_tprintf(TEXT("[Erro]Criação de objectos do Windows(%d)\n"), GetLastError());
+		return -1;
+	}
+	if (GetLastError() != ERROR_ALREADY_EXISTS) {
+
+		init = 1;
+
+	}
+
+	shm = (bufferinfo*)MapViewOfFile(hMemoria, FILE_MAP_WRITE, 0, 0, sizeof(bufferinfo));
+
+	if (shm == NULL) {
+		_tprintf(TEXT("[Erro]Mapeamento da memória partilhada(%d)\n"), GetLastError());
+		return -1;
+	}
+
+	if (init) {
+
+		shm->iEscrita = 0;
+		shm->iLeitura = 0;
+		ReleaseSemaphore(PodeEscrever, 10, NULL);
+	}
+	for (int i = 0; i < 1; i++)
+	{
+		WaitForSingleObject(PodeEscrever, INFINITE);
+
+		//METER O MUTEX PARA PROTEGER A ESCRITA -> PODE HAVER MA INFORMACAO PARA OS CLIENTES QUANDO SE LIBERTA O SEMAFORO 
+		//DE LEITURA. ESCREVE COM INDICE > primeiro...
+		WaitForSingleObject(mutex, INFINITE);
+		//ler IN par aa var local POS
+		pos = shm->iEscrita;
+		shm->iEscrita = (shm->iEscrita + 1) % Buffers;
+		//Incrementar valor de IN
+
+
+		shm->dados[pos].tipo = tipo;
+		shm->dados[pos].aux1 = aux1;
+		shm->dados[pos].aux2 = aux2;
+		shm->dados[pos].aux3 = aux3;
+		shm->dados[pos].aux4 = aux4;
+		shm->dados[pos].aux5 = aux5;
+		//_stprintf_s(shm->buff[pos], BufferSize, TEXT("Pedido %d#%02d"), GetCurrentProcessId(), i);
+		//_tprintf(TEXT("Escrever para buffer %d o valor %d \n"), pos, shm->iEscrita);
+		_tprintf(TEXT("Escrever MSG para buffer circular\n"));
+
+		ReleaseMutex(mutex);
+
+		Sleep(1000);
+		ReleaseSemaphore(PodeLer, 1, NULL);
+	}
+
+	UnmapViewOfFile(shm);
+	CloseHandle(PodeEscrever);
+	CloseHandle(PodeLer);
+	CloseHandle(hMemoria);
+	CloseHandle(mutex);
+	return 0;
+}
+
+void ler(obj *objectos) {
+	int i;
+	for (i = 0;objectos[i].id != NULL;i++) {
+		printf("	id: %d\n	tipo: %d\n	x: %d\n	y: %d\n	Largura: %d\n	Altura: %d\n\n	********************************\n", objectos[i].id, objectos[i].tipo, objectos[i].x, objectos[i].y, objectos[i].tamx, objectos[i].tamy);
+	}
+}
+
+obj * mapeamento() {
+	TCHAR syNome[] = TEXT("Global\\GlobalGameData");
+	HANDLE partilha;
+	obj *ponteiro;
+
+	partilha = CreateFileMapping(INVALID_HANDLE_VALUE,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		sizeof(obj) * 300,
+		syNome);
+
+	if (partilha == NULL) {
+		_tprintf(TEXT("Nao foi possifel criar o mapeamento no systema, problemas de permissao ERRO ""5"" (%d).\n"), GetLastError());
+		return NULL;
+	}
+
+	ponteiro = (obj *)MapViewOfFile(partilha,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		sizeof(obj) * 300);
+
+	if (ponteiro == NULL)
+		_tprintf(TEXT("Nao foi possivel fazer o mapeamento do vector no espaco mapeado ERRO (%d)\n"), GetLastError());
+
+	return ponteiro;
+}
---
+++ EspaceInvadersSO2/Server/Source.cpp
@@ -0,0 +1,264 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <io.h>
+#include <fcntl.h>
+#include <time.h>
+#include <tchar.h>
+
+#define MAPX = 1920
+#define MAPY = 1080
+#define BufferSize 100
+#define Buffers 10
+
+typedef struct obj {
+	int id;
+	int tipo;
+	int x;
+	int y;
+	int tamx;
+	int tamy;
+	char bitmap[1024];
+	char nome[256];
+	struct obj * prox;
+}obj;
+typedef struct {
+	int tipo;
+	int aux1;
+	int aux2;
+	int aux3;
+	int aux4;
+	int aux5;
+}msg;
+typedef struct {
+
+	msg dados[Buffers];
+	int iEscrita;
+	int iLeitura;
+
+}bufferinfo;
+
+int objid;
+
+DWORD WINAPI criaojogo(LPVOID lparam);
+int criaobj(obj * objectos, int tipo, int x, int y, int tamx, int tamy);
+int listaobjectos(obj * objectos);
+int apagaobjecto(obj * objectos, int id);
+obj * mapeamento();
+int buffercircular();
+int tratamsg(msg data);
+
+int _tmain() {     // main main main main main main main main main main main main main main main main main main main main
+	int a;
+	obj * objectos;
+	objid = 0;
+	objectos = mapeamento();
+	while (1)
+	{
+		printf("\n	Servidor\n");
+		printf("\n	Opcoes\n\n");
+		printf("[ 1 ] -> Cria objecto\n");
+		printf("[ 2 ] -> Lista objecto\n");
+		printf("[ 3 ] -> BufferCircular\n");
+		printf("[ 4 ] -> Apaga objecto\n");
+		printf("[ 5 ] -> Sair\n");
+		scanf_s("%d", &a);
+		switch (a)
+		{
+		case 1:
+			//criaobj(objectos);
+			break;
+		case 2:
+			system("cls");
+			listaobjectos(objectos);
+			system("pause");
+			break;
+		case 3:
+			buffercircular();
+			system("pause");
+			break;
+		case 4:
+			//apagaobjecto(objectos);
+			system("pause");
+			break;
+		case 5:
+			return 0;
+			break;
+		default:
+			break;
+		}
+		system("cls");
+	}
+
+
+}
+
+int buffercircular() {
+
+	TCHAR NomeMemoria[] = TEXT("Nome da Memória Partilhada");
+	TCHAR NomeSemaforoPodeEscrever[] = TEXT("Semáforo Pode Escrever");
+	TCHAR NomeSemaforoPodeLer[] = TEXT("Semáforo Pode Ler");
+
+	TCHAR NomeMutexIndice[] = TEXT("MutexLeitor");
+
+	HANDLE PodeEscrever;
+	HANDLE PodeLer;
+	HANDLE hMemoria;
+	HANDLE mutex;
+	while (1) {
+
+		bufferinfo *shm;
+		int pos;
+
+		PodeEscrever = CreateSemaphore(NULL, Buffers, Buffers, NomeSemaforoPodeEscrever);
+		PodeLer = CreateSemaphore(NULL, 0, Buffers, NomeSemaforoPodeLer);
+		hMemoria = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(bufferinfo), NomeMemoria);
+
+		mutex = CreateMutex(NULL, FALSE, NomeMutexIndice);
+
+		if (PodeEscrever == NULL || PodeLer == NULL || hMemoria == NULL) {
+			_tprintf(TEXT("[Erro]Criação de objectos do Windows(%d)\n"), GetLastError());
+			return -1;
+		}
+
+		shm = (bufferinfo*)MapViewOfFile(hMemoria, FILE_MAP_WRITE, 0, 0, sizeof(bufferinfo));
+		if (shm == NULL) {
+			_tprintf(TEXT("[Erro]Mapeamento da memória partilhada(%d)\n"), GetLastError());
+			return -1;
+		}
+
+		for (int i = 0;; ++i) {
+			WaitForSingleObject(PodeLer, INFINITE);
+
+			WaitForSingleObject(mutex, INFINITE);
+			//ler IN par aa var local POS
+			pos = shm->iLeitura;
+			shm->iLeitura = (shm->iLeitura + 1) % Buffers;//adiciona ate que é == a Buffers
+														  //Incrementar valor de IN
+			ReleaseMutex(mutex);
+
+			tratamsg(shm->dados[pos]);// copia data do buffer para variavel da funcao e liberta o buffer
+			ReleaseSemaphore(PodeEscrever, 1, NULL);
+
+		}
+
+		UnmapViewOfFile(shm);
+		CloseHandle(PodeEscrever);
+		CloseHandle(PodeLer);
+		CloseHandle(hMemoria);
+		CloseHandle(mutex);
+	}
+	return 0;
+}
+
+int tratamsg(msg data) {
+	HANDLE semaforo1 = CreateSemaphore(NULL, 1, 1, TEXT("semaforo1"));
+	WaitForSingleObject(semaforo1, INFINITE);
+	switch (data.tipo) {
+	case 1:
+		CreateThread(NULL, 0, criaojogo, (LPVOID)&data, 0, NULL);
+		break;
+	case 2:
+
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	default:
+		break;
+	}
+	WaitForSingleObject(semaforo1, INFINITE);
+	ReleaseSemaphore(semaforo1, 1, NULL);
+	return 0;
+}
+
+DWORD WINAPI criaojogo(LPVOID lparam) {
+	msg *pdata = (msg *)lparam;
+	msg data = *pdata;
+	HANDLE semaforo1 = CreateSemaphore(NULL, 1, 1, TEXT("semaforo1"));
+	ReleaseSemaphore(semaforo1, 1, NULL);
+
+	obj * objectos = mapeamento();
+	int i;
+	for (i = 0;i < 30;i++) {
+		criaobj(objectos, 1, i + 10, i + 20, 10, 10);
+	}
+	return 0;
+}
+
+int criaobj(obj * objectos, int tipo, int x, int y, int tamx, int tamy) {
+	int i;
+
+	for (i = 0;objectos[i].id != NULL;i++) {
+	}
+	objid++;
+	objectos[i].id = objid;
+	objectos[i].tipo = tipo;
+	objectos[i].x = x;
+	objectos[i].y = y;
+	objectos[i].tamx = tamx;
+	objectos[i].tamy = tamy;
+	return 0;
+}
+
+int listaobjectos(obj * objectos) {
+	int i;
+	for (i = 0;objectos[i].id != NULL;i++) {
+		printf("	id: %d\n	tipo: %d\n	x: %d\n	y: %d\n	Largura: %d\n	Altura: %d\n\n	********************************\n", objectos[i].id, objectos[i].tipo, objectos[i].x, objectos[i].y, objectos[i].tamx, objectos[i].tamy);
+	}
+	return 0;
+}
+
+obj * mapeamento() {
+	TCHAR syNome[] = TEXT("Global\\GlobalGameData");
+	HANDLE partilha;
+	obj *ponteiro;
+
+	partilha = CreateFileMapping(INVALID_HANDLE_VALUE,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		sizeof(obj) * 300,
+		syNome);
+
+	if (partilha == NULL) {
+		_tprintf(TEXT("Nao foi possifel criar o mapeamento no systema, problemas de permissao ERRO ""5"" (%d).\n"), GetLastError());
+		return NULL;
+	}
+
+	ponteiro = (obj *)MapViewOfFile(partilha,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		sizeof(obj) * 300);
+
+	if (ponteiro == NULL)
+		_tprintf(TEXT("Nao foi possivel fazer o mapeamento do vector no espaco mapeado ERRO (%d)\n"), GetLastError());
+
+	return ponteiro;
+}
+
+int apagaobjecto(obj * objectos, int id) {
+	int i, e;
+
+	for (i = 0;objectos[i].id != NULL;i++) {
+		if (objectos[i].id == id) {
+			for (e = i;objectos[e].id != NULL;e++) {
+				objectos[e].id = objectos[e + 1].id;
+				objectos[e].tipo = objectos[e + 1].tipo;
+				objectos[e].x = objectos[e + 1].x;
+				objectos[e].y = objectos[e + 1].y;
+				objectos[e].tamx = objectos[e + 1].tamx;
+				objectos[e].tamy = objectos[e + 1].tamy;
+			}
+			return 1;
+		}
+	}
+
+	return 0;
+}
